e29844fd1e90cf59963aa23f255ab4233d4aed53
luca-schröder <janlschroede@uos.de>
2023-08-30 12:30:30 +0200

    Added curved lines option
    Added an option for the lines between the connected regions to be curved

--- connCirclePlot.m
+++ connCirclePlot.m
@@ -17,18 +17,20 @@ classdef connCirclePlot < handle
 % TextSize: A numeric value for the min value for the text size of the
 % labels.
 % MaxLineThickness  : A numeric value for the max thickness the lines.
-% ShowColorbar: Boolean value that indicates whether to show the colorbar
+% ShowColorbar: Boolean value that indicates whether to show the colorbar.
+% CurvedLines: Boolean value that indicates whether to plot a curve or a
+% line.
 %%
-% Author: Jan-Luca Schröder
+% Author: Jan-Luca Schröder (2023)
 
     properties
-%         Node = node(0,0); % Array of nodes
         Label;            % Cell array of strings
         MaxVal;           % Numeric value
         MinVal;           % Numeric value
         ShowColorbar;     % Boolean value
         TextSize;         % Numeric value
         MaxLineThickness; % Numeric value  
+        CurvedLines;      % Boolean value
     end
 
     methods
@@ -36,8 +38,8 @@ classdef connCirclePlot < handle
 
             p = inputParser;
 
-            % Daniel's r-b-w colormap:
-            load('/mnt/storage/tier2/danbal/BRACHR002N3B/Luca/MyColormaps', 'rbw_db2');
+            % load colourmap
+            load('./', 'rbw_cmap');
             
             % set default label
             defaultLabel = cell(length(adjacencyMatrix),1);
@@ -47,6 +49,9 @@ classdef connCirclePlot < handle
 
             % turn on colorbar by default
             defaultShowColorbar = true;
+            
+            % curve lines by defaults
+            defaultCurvedLines = true;
 
             % set default min and max values for colormap
             defaultMin = -1;
@@ -62,7 +67,8 @@ classdef connCirclePlot < handle
             addParameter(p,'ShowColorbar' ,defaultShowColorbar ,@islogical);
             addParameter(p,'TextSize' ,defaultTextSize ,@isnumeric);
             addParameter(p,'MaxLineThickness' ,defaultLineWidth ,@isnumeric);
-
+            addParameter(p,'CurvedLines' ,defaultCurvedLines ,@islogical);
+            
             parse(p,adjacencyMatrix,varargin{:});
 
             this.Label      = p.Results.Label;
@@ -71,6 +77,8 @@ classdef connCirclePlot < handle
             this.TextSize   = p.Results.TextSize;
             this.ShowColorbar     = p.Results.ShowColorbar;
             this.MaxLineThickness = p.Results.MaxLineThickness;
+            this.CurvedLines    = p.Results.CurvedLines;
+
 
             theta=linspace(0,2*pi,length(adjacencyMatrix)+1);
             theta=theta(1:end-1);
@@ -83,8 +91,6 @@ classdef connCirclePlot < handle
             % [minDistance, indexOfMin] = min(abs(colourmap-adjacencyMatrix(1,3)));
             
             % plot nodes
-%             h=figure;
-%             clf(h);
             plot(x,y,'.k','markersize',20, 'Color', [0.5 0.5 0.5]);hold on
 
             % [minDistance, indexOfMin] = min(abs(V-N));
@@ -119,22 +125,49 @@ classdef connCirclePlot < handle
             for i= 1:length(ind1)
                 % compare indices to colormap
                 [~, indexOfMin] = min(abs(colourmap-adjacencyMatrix(ind1(i),ind2(i))));
-                colVal = rbw_db2(indexOfMin,:);
+                colVal = rbw_cmap(indexOfMin,:);
                 % set linewidth based on limits
                 lineWdt = lineWidthBar(indexOfMin);
-                % draw a line
-%                 disp([cos(x(ind1(i))),sin(x(ind2(i)))])
-                line([x(ind1(i)),x(ind2(i))],...
+                
+                % draw line or curve
+                if this.CurvedLines
+                    % end points
+                    X = [x(ind1(i)) x(ind2(i))];
+                    Y = [y(ind1(i)) y(ind2(i))];
+                    % intermediate point
+                    Xi = mean([x(ind1(i)) x(ind2(i))]) - 0.2*mean([x(ind1(i)) x(ind2(i))]);
+                    Yi = mean([y(ind1(i)) y(ind2(i))]) - 0.2*mean([y(ind1(i)) y(ind2(i))]);
+                    
+                    % create new 3-coord point
+                    Xa = [x(ind1(i)) Xi x(ind2(i))];
+                    Ya = [y(ind1(i)) Yi y(ind2(i))];
+                    
+                    % create grided steps in line
+                    t  = 1:numel(Xa);
+                    ts = linspace(min(t),max(t),numel(Xa)*10);
+                    xx = spline(t,Xa,ts);
+                    yy = spline(t,Ya,ts);
+                    
+                    % plot curve
+                    plot(xx,yy,...
+                        'Color', colVal,...
+                        'LineWidth', lineWdt); hold on; % curve
+%                     plot(Xi,Yi,'xr')      % intermediate point
+
+                else
+                    % plot line
+                    line([x(ind1(i)),x(ind2(i))],...
                     [y(ind1(i)),y(ind2(i))],...
                     'Color',colVal,...
                     'LineWidth',lineWdt);
+                end
                 %abs(adjacencyMatrix(ind1(i),ind2(i))));
             end
             
             % set colormap to show on figure according to the 
             % max and min values given
             if this.ShowColorbar
-                colormap(rbw_db2)
+                colormap(rbw_cmap)
                 hCB = colorbar('eastoutside');
                 hCB.Position = [0.125 0.05 0.75 0.03];
                 hCB.Location = 'eastoutside';% TEMP - originally'eastoutside';
